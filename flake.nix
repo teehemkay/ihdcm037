6c3c264dcb188c93ed8cc8483c3f8aa9192c698a
MABONZO Kalimasi Nzau-Mfuka <kalimasi.mabonzo@ext.europarl.europa.eu>
2024-08-20 19:46:43 +0200

    chore: setup flake-based dev env

--- flake.nix
+++ flake.nix
@@ -1,29 +1,32 @@
 {
-  description = "IHDCM037 jupyenv project";
+  description = "frontend standard development environment";
 
-  nixConfig.extra-substituters = [ "https://tweag-jupyter.cachix.org" ];
-  nixConfig.extra-trusted-public-keys = [
-    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
-  ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default-darwin";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.flake-compat.url = "github:edolstra/flake-compat";
-  inputs.flake-compat.flake = false;
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.jupyenv.url = "github:tweag/jupyenv";
+    # Override flake-utils systems to mine
+    flake-utils.inputs.systems.follows = "systems";
+  };
 
-  outputs = { self, flake-compat, flake-utils, nixpkgs, jupyenv, ... }@inputs:
-    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-darwin ] (system:
+  outputs = { nixpkgs, systems, flake-utils, ... }:
+    let
+      eachSystem = f:
+        flake-utils.lib.eachSystem (import systems)
+        (system: f nixpkgs.legacyPackages.${system});
+
+    in eachSystem (pkgs:
       let
-        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
-        jupyterlab = mkJupyterlabNew ({ ... }: {
-          nixpkgs = inputs.nixpkgs;
-          imports = [ (import ./kernels.nix) ];
-        });
-      in rec {
-        packages = { inherit jupyterlab; };
-        packages.default = jupyterlab;
-        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
-        apps.default.type = "app";
+        inherit (pkgs) stdenv lib mkShell;
+        inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation CoreServices;
+
+      in {
+        devShells = {
+          default = mkShell {
+            packages = [ pkgs.hy pkgs.python3 ]
+              ++ lib.optionals stdenv.isDarwin [ CoreFoundation CoreServices ];
+          };
+        };
       });
 }
--- kernels.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-  kernel.python.ihdcm037 = {
-    enable = true;
-    displayName = "Machine Learning - UNamur IHDCM037";
-    extraPackages = ps: [ ps.scikit-learn ];
-  };
-}
